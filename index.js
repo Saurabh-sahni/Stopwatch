3433c1431219d06e59b1c74c7b6474aabc5d5b27
Saurabh-sahni <saurabhsahni833gmail.com>
2021-10-08 01:58:06 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,66 @@
+// Convert to time format 
+
+function timeToString(time) {
+    let diffInHrs = time / 3600000;
+    let hh = Math.floor(diffInHrs);
+
+    let diffInMin = (diffInHrs - hh) * 60;
+    let mm = Math.floor(diffInMin);
+
+    let diffInSec = (diffInMin - mm) * 60;
+    let ss = Math.floor(diffInSec);
+
+    let diffInMs = (diffInSec - ss) * 100;
+    let ms = Math.floor(diffInMs);
+
+    let formattedMM = mm.toString().padStart(2, "0");
+    let formattedSS = ss.toString().padStart(2, "0");
+    let formattedMS = ms.toString().padStart(2, "0");
+
+    return `${formattedMM}min ${formattedSS}sec ${formattedMS}ms`;
+}
+
+
+let startTime;
+let elapsedTime = 0;
+let timerInterval;
+
+
+function print(txt) {
+  document.getElementById("display").innerHTML = txt;
+}
+
+// Create "start", "pause" and "reset" functions
+
+function start() {
+  startTime = Date.now() - elapsedTime;
+  timerInterval = setInterval(function printTime() {
+    elapsedTime = Date.now() - startTime;
+    print(timeToString(elapsedTime));
+  }, 10);
+  showButton("PAUSE");
+}
+
+function pause() {
+  clearInterval(timerInterval);
+  showButton("PLAY");
+}
+
+function reset() {
+  clearInterval(timerInterval);
+  print("00min 00sec 00ms");
+  elapsedTime = 0;
+  showButton("PLAY");
+}
+
+//function to display button
+let playButton = document.getElementById("start");
+let pauseButton = document.getElementById("pause");
+
+
+function showButton(buttonKey) {
+  const buttonToShow = buttonKey === "PLAY" ? playButton : pauseButton;
+  const buttonToHide = buttonKey === "PLAY" ? pauseButton : playButton;
+  buttonToShow.style.display = "inline-block";
+  buttonToHide.style.display = "none";
+}
